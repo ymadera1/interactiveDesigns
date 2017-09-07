e9c34044b8334e0df8d88d2051f8c3a835220213
yvonne <yvonne_madera@yahoo.com>
2017-09-06 20:11:19 -0400

    first website upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Set up the nav scrolling effect
+$("nav").onePageNav();
+
+
+
+
+	var menu = $("nav ul");
+
+	// Toggle the menu when clicked
+	$("#menu-icon").click(function() {
+	menu.slideToggle();
+	});
+
+
+
+//$("button #hire-me").slideToggle();
+
+
+// Fix bug where menu disappears if you open and close it on a small screen
+// When I resize the window
+$(window).resize(function() {
+	var width = $(window).width();
+
+	// if window width is > 768 and the menu is hidden
+	if(width > 768 && menu.is(":hidden")) {
+		menu.removeAttr("style");
+	}
+
+});
+
+// Add parallax (stellar.js)
+$.stellar();
+
+// Add wow.js animations
+new WOW().init();
